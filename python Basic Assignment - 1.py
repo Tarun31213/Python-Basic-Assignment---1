13d84aebc95bb68e1b4a7b1713795ba35244050f
Tarun31213 <95556291+Tarun31213@users.noreply.github.com>
2022-03-04 19:33:46 +0530

    Add files via upload

---
+++ python Basic Assignment - 1.py
@@ -0,0 +1,37 @@
+1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+* 
+'hello'
+-87.8
+- 
+/ 
++	
+6 
+# Ans.1:- 'hello' , -87.7 , 6 are the vaalues
+        # - , / , + , * are the expression 
+2. what is the difference between string and variable ?
+# Ans.2:- String:- all the variables and constants under paranthesis[""] are known as 'STRING'
+        # Variable:- the alphabetic character that expresses a numerical value or a number
+3. Describe three different data types ?
+# Ans.3:- 'int' :- all integers comes under 'int' data-type
+       # 'float' :- all decimal and complex numbers comes under 'float' data-type
+       # 'str' :- all the variables or alphabets comes under 'str' data-type
+4. What is an expression made up of? What do all expressions do?     
+# Ans.4:- Expressions are made up of 'Terms' which are represented by unknown variables,constants and coefficients. It is used to evaluate to a single value
+5.This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+# Ans.5:-  Statements do something while expressions are representation of value
+6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+# Ans.6:- 23
+7. What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam' * 3
+# Ans.7:- 'spamspamspam'
+8. Why is eggs a valid variable name while 100 is invalid?
+# Ans.8:- because eggs is an variable of alphabets whereas , 100 is numerical 
+9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+# Ans.9:- str() , int() , float()
+10. Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'
+# Ans.10:- Because two dissimilar data-types cannot be concatenated in order to concatenate it needs to be done type-casting
+
